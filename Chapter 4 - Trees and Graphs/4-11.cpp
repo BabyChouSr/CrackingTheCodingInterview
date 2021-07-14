80641185fb77c8879b4c2a53ff249c9b0d55840e
BabyChouSr <emu4yolife@gmail.com>
2021-07-14 00:15:49 -0700

    4-11

--- Chapter 4 - Trees and Graphs/4-11.cpp
+++ Chapter 4 - Trees and Graphs/4-11.cpp
@@ -5,6 +5,8 @@
 
 #include <iostream>
 #include <queue>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
@@ -20,8 +22,9 @@ struct Node {
 
 struct BST {
     Node* root;
-    BST() : root() {}
-    BST(Node* root) : root(root) {}
+    int size;
+    BST() : root(), size(0) {}
+    BST(Node* root) : root(root), size(1) {}
 
     int calcHeight(Node* n) {
         if (n == nullptr) {return 0;}
@@ -59,13 +62,16 @@ struct BST {
     }
 
     Node* insert(Node* node, int value) {
-        if (node == nullptr) {return new Node(value);}
+        if (node == nullptr) {
+            size++;
+            return new Node(value);
+        }
 
         if (value > node->val) {
-            return insert(node->right, value);
+            node->right = insert(node->right, value);
         }
         else if (value <= node->val) {
-            return insert(node->left, value);
+            node->left = insert(node->left, value);
         }
 
         node->height = calcHeight(node);
@@ -121,66 +127,161 @@ struct BST {
         }
         return current;
     }
-    Node* deleteNode(Node* root, int value) {
-        if (root == nullptr) {return root;}
-        if (value > root->val) {
-            return deleteNode(root->right, value);
-        } else if (value < root->val) {
-            return deleteNode(root->left, value);
+    Node* deleteNode(Node* node, int value) {
+        if (node == nullptr) {
+            return node;
+        }
+        if (value > node->val) {
+            node->right = deleteNode(node->right, value);
+        } else if (value < node->val) {
+            node->left = deleteNode(node->left, value);
         } else { // This is the node that has to be deleted
-            if (root->left == nullptr || root->right == nullptr) {
-                Node* temp = root->left ? root->left : root->right;
+            size--;
+            if (node->left == nullptr || node->right == nullptr) {
+                Node* temp = node->left ? node->left : node->right;
                 if (temp == nullptr) { // No child case
-                    temp = root;
-                    root = nullptr;
+                    temp = node;
+                    node = nullptr;
                 }
                 else { // One child
-                    *root = *temp; // Copy the contents of that one child
+                    *node = *temp; // Copy the contents of that one child
                 }
-                delete temp;
+                free(temp);
             }
             else { // If there are two children
-                Node* temp = miniValueNode(root->right); // Gives some random node
-                root->val = temp->val;
-                root->right = deleteNode(root->right, temp->val);
+                Node* temp = miniValueNode(node->right); // Gives the inorder successor
+                node->val = temp->val;
+                node->right = deleteNode(node->right, temp->val);
             }
         }
-
-        if (root == nullptr) {
-            return root;
+        if (node == nullptr) {
+            return node;
         }
-        root->height = calcHeight(root);
-        int balance = balanceFactor(root);
+        node->height = calcHeight(node);
+        int balance = balanceFactor(node);
 
         // Left Left Case
-        if (balance > 1 && value < root->left->val) {
-            return rightRotate(root);
+        if (balance > 1 && value < node->left->val) {
+            return rightRotate(node);
         }
 
         // Right Right Case
-        if (balance < -1 && value > root->right->val) {
-            return leftRotate(root);
+        if (balance < -1 && value > node->right->val) {
+            return leftRotate(node);
         }
 
         // Left Right Case
-        if (balance > 1 && value > root->left->val) {
-            root->left = leftRotate(root->left);
-            return rightRotate(root);
+        if (balance > 1 && value > node->left->val) {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
         }
         
         // Right Left Case
-        if (balance < -1 && value < root->right->val) {
-            root->right = rightRotate(root->right);
-            return leftRotate(root);
+        if (balance < -1 && value < node->right->val) {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
         }
-        return root;
+        return node;
         
 
     }
 
+    int calcSize(Node* n) {
+        if (n == nullptr) {return 0;}
+        return 1 + calcSize(n->left) + calcSize(n->right);
+    }
+
+    // Time Complexity: O(N) since it may traverse entire tree
+    // Space Complexity: O(N)
+    Node* getRandomNode() {
+        srand (time(NULL));
+        int random_number = rand() % size;
+        cout << random_number << endl;
+        int count = 0;
+        queue <Node*> q;
+        q.push(root);
+        while (!q.empty()) {
+            Node* x = q.front();
+            q.pop();
+            if (count == random_number) {
+                return x;
+            } 
+            if (x->left != nullptr) {q.push(x->left);}
+            if (x->right != nullptr) {q.push(x->right);}
+            count++;
+        }
+        return nullptr;
+    }
+
+    // Time Complexity: O(log(N))
+    Node* getRandomNode2(Node* node){
+        // NORMALLY WE WOULD HAVE THE SIZE STORED IN THE NODE CLASS ITSELF BUT 
+        // TO SIMULATE MORE EASILY, I WROTE A SIZE() function
+        srand (time(NULL));
+        int randomNumber = rand() % calcSize(node);
+        int leftSize = left == nullptr ? 0 : calcSize(node->left);
+        if (randomNumber < leftSize) {
+            return getRandomNode2(node->left);
+        }   else if (randomNumber == leftSize) {
+            return node;
+        } else {
+            return getRandomNode2(node->right);
+        }
+    }
+
+    Node* getIthNode(Node* node, int index) {
+        int leftSize = root->left == nullptr ? 0 : calcSize(root->left);
+        if (index < leftSize) {
+            return getIthNode(node->left, index);
+        } else if (index == leftSize) {
+            return node;
+        } else {
+            return getIthNode(node->right, index - (leftSize + 1));
+        }
+
+    }
+
+
+    // Time Complexity: O(log(N))
+    // BENEFITS: Don't need to continuously call the random generator
+    Node* getRandomNode3(Node* node) {
+        srand(time(NULL));
+        int randomNumber = rand() % calcSize(node);
+        return getIthNode(node, randomNumber);
+    }
+
 };
 
-int main() {
+void preOrder(Node* n) {
+    if (n == nullptr) {return;}
+    cout << n->val << endl;
+    if (n->left != nullptr) {preOrder(n->left);}
+    if (n->right != nullptr) {preOrder(n->right);}
+}
 
+int main() {
+    BST* bst = new BST(new Node(5));
+    bst->root = bst->insert(bst->root, 10);
+    bst->root = bst->insert(bst->root, 15);
+    bst->root = bst->insert(bst->root, 20);
+    bst->root = bst->insert(bst->root, 25);
+    bst->root = bst->insert(bst->root, 30);
+
+    preOrder(bst->root);
+    cout << bst->size << endl;
+
+    cout << "POST DELETION OF 15:" << endl;
+    bst->root = bst->deleteNode(bst->root, 15);
+    preOrder(bst->root);
+    cout << bst->size << endl;
+
+    // cout << "GET RANDOM NODE 1" << endl;
+    // cout << bst->getRandomNode()->val << endl;
+
+    // cout << "GET RANDOM NODE 2" << endl;
+    // cout << bst->getRandomNode2(bst->root)->val << endl;
+
+    cout << "GET RANDOM NODE 3" << endl;
+    cout << bst->getRandomNode2(bst->root)->val << endl;
     return 0;
 }
